b612dc0da42cbd761d6ff4998568455f7ba8fbf2
slembcke <slembcke@gmail.com>
2026-02-01 19:51:48 -0600

    game done-ish

--- audio/audio.s
+++ audio/audio.s
@@ -62,14 +62,16 @@ nmi_callback:
 
 .rodata
 
+.export _SOUNDS
+_SOUNDS:
+	.include "sounds.s"
+
+.segment "PRG0"
+
 FT_DPCM_PTR = (FT_DPCM_OFF & $3fff) >> 6
 FT_DPCM_OFF:
 	; .incbin "after_the_rain.dmc"
 
 .export _MUSIC
 _MUSIC:
-	; .include "after_the_rain.s"
-
-.export _SOUNDS
-_SOUNDS:
-	.include "sounds.s"
+	.include "after_the_rain.s"
--- src/main.c
+++ src/main.c
@@ -434,8 +434,13 @@ static void update_arena(void){
 						drop_trash(idx);
 
 						// Check for matching goal
-						if (matching) { add_score_player1(GOAL_TRASH_SCORE); player1.perfect_sparkle = true; }
-						else		    add_score_player1(NORMAL_TRASH_SCORE);
+						if (matching) {
+							sound_play(1);
+							add_score_player1(GOAL_TRASH_SCORE); player1.perfect_sparkle = true;
+						} else {
+							sound_play(3);
+							add_score_player1(NORMAL_TRASH_SCORE);
+						}
 					} else if (ARENA.trash.x[idx] / 256 > DUMPSTER_2_LEFT_BOUND){
 						bool matching = ARENA.trash.type[idx] == player2.goal_trash_type;
 	
@@ -734,7 +739,8 @@ static void update_player_movement(){
 			player1.hands_free = false;
 		}
 		
-		if(JOY_BTN_A(pad1.release)){
+		if(JOY_BTN_A(pad1.release) && player1.hands_free == false){
+			// sound_play(0);
 			ARENA.trash.throw_anim[player1.selected] = THROW_TICKS;
 			ARENA.trash.y[player1.selected] += 256*16;
 			ARENA.trash.throw_dir[player1.selected] = player1.player_direction;
@@ -786,7 +792,8 @@ static void update_player_movement(){
 			player2.hands_free = false;
 		}
 		
-		if(JOY_BTN_A(pad2.release)){
+		if(JOY_BTN_A(pad2.release) && player2.hands_free == false){
+			// sound_play(0);
 			ARENA.trash.throw_anim[player2.selected] = THROW_TICKS;
 			ARENA.trash.y[player2.selected] += 256*16;
 			ARENA.trash.throw_dir[player2.selected] = player2.player_direction;
@@ -1179,7 +1186,7 @@ void main(void){
 	for(idx = 0; idx < 32; idx++) px_buffer_set_color(idx, 0x1D);
 	px_wait_nmi();
 	
-	// music_init(&MUSIC);
+	music_init(&MUSIC);
 	sound_init(&SOUNDS);
 	// music_play(0);
 	
@@ -1187,7 +1194,7 @@ void main(void){
 	px_debug_hex_addr = NT_ADDR(0, 3, 3);
 	
 	// Jump to the splash screen state.
-	igda_screen();
-	// game_run();
+	// igda_screen();
+	game_run();
 	
 }
